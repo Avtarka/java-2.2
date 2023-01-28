70e8aac29052e1f25ec0d3589bba5373773ee981
Pavel Kryuchkov <mr.p@bk.ru>
2023-01-28 23:48:54 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cheсkClient = 100; // сумма счёта клиента
+        int addCash = 1100; // сумма пополнения
+        int bonus = 1; // стоимость бонуса (руб.)
+
+        boolean border = true; // порог 1000р.
+
+        if (border) {
+            (addCash < 1000)
+        } else {
+            (addCash > 1000)
+        }
+        System.out.println(+ bonus);
+    }
+
+
+        System.out.println("Сумма зачисления на счёт:");
+        System.out.println(cheсkClient + addCash + bonus + "руб.");
+
+        System.out.println("Сумма бонуса:" + bonus + "руб.");
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
